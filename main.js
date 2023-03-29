6da6037886ca465b6644d2991d1de480702b26ad
Arun8008 <arun.sivanpandian.2000@gmail.com>
2023-03-29 15:15:19 +0530

    update

---
+++ main.js
@@ -0,0 +1,39 @@
+let num=345
+let c=0
+
+while(num>0){
+    var b=num%10
+    num=(num-b)/10
+    if(b==0){
+        document.write("Zero")
+    }
+    else if(b==1){
+        document.write("One")
+    }
+    else if(b==2){
+        document.write("Two")
+    }
+    else if(b==3){
+        document.write("Three")
+    }
+    else if(b==4){
+        document.write("Four")
+    }
+    else if(b==5){
+        document.write("Five")
+    }
+    else if(b==6){
+        document.write("Six")
+    }
+    else if(b==7){
+        document.write("Seven")
+    }
+    else if(b==8){
+        document.write("Eight")
+    }
+    else if(b==9){
+        document.write("Nine")
+    }
+   
+
+}
